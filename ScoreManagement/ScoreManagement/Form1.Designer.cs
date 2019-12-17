e82c1140048672552321a12cfc5485c250c52b51
187PM14008 - Ho Tan Minh Tam - K24T-PM1 <tam.187pm14008@vanlanguni.vn>
2019-12-17 15:37:28 +0700

    ok

--- ScoreManagement/ScoreManagement/Form1.Designer.cs
+++ ScoreManagement/ScoreManagement/Form1.Designer.cs
@@ -36,11 +36,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(276, 132);
+            this.label1.Location = new System.Drawing.Point(332, 132);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(366, 37);
+            this.label1.Size = new System.Drawing.Size(281, 37);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Quản lý điểm sinh viên";
+            this.label1.Text = "Quản lý sinh viên";
             // 
             // btnStart
             // 
